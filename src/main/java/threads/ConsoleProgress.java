30b96f33b2621e54077d39a741192fddb6e4596a
AlexanderBanar <s595659@mail.ru>
2021-06-02 07:56:55 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/threads/ConsoleProgress.java
@@ -0,0 +1,37 @@
+package threads;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        String[] symbols = getSymbols();
+        int symbolCount = 0;
+        while (!Thread.currentThread().isInterrupted()
+        && count < 101) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("\rLoading... %s, progress: %d%%", symbols[symbolCount++], count++);
+            if (symbolCount == 3) {
+                symbolCount = 0;
+            }
+        }
+    }
+
+    private String[] getSymbols() {
+        String[] symbols = new String[3];
+        symbols[0] = "(  ";
+        symbols[1] = "  )";
+        symbols[2] = " _ ";
+        return symbols;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(8000);
+        progress.interrupt();
+    }
+}
