b53027aaa1a327c83a843aaef8a6966921afff83
AlexanderBanar <s595659@mail.ru>
2021-06-02 07:03:47 +0300

    1.1. Состояние нити. [#229175 #162882] _ refactored

--- src/main/java/threads/ThreadState.java
+++ src/main/java/threads/ThreadState.java
@@ -1,7 +1,7 @@
 package threads;
 
 public class ThreadState {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread zero = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
@@ -10,12 +10,12 @@ public class ThreadState {
         );
         zero.start();
         first.start();
-        Thread.sleep(1000);
-        if (zero.getState() == Thread.State.TERMINATED
-                && first.getState() == Thread.State.TERMINATED) {
-            System.out.println();
-            System.out.printf("This is Thread: %s\r\n", Thread.currentThread().getName());
-            System.out.println("The zero thread and the first threads are terminated");
+        while (zero.getState() != Thread.State.TERMINATED
+                || first.getState() != Thread.State.TERMINATED) {
+            System.out.println("the branch threads are still running...");
         }
+        System.out.println();
+        System.out.printf("This is Thread: %s\r\n", Thread.currentThread().getName());
+        System.out.println("The zero thread and the first threads are terminated");
     }
 }
