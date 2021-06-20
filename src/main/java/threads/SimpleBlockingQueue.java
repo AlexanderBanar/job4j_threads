30144bee3f1f41fade4777e24696d532bc08b800
AlexanderBanar <s595659@mail.ru>
2021-06-20 09:49:09 +0300

    1. Реализовать шаблон Producer Consumer. [#1098 #168902] _ refactored

--- src/main/java/threads/SimpleBlockingQueue.java
+++ src/main/java/threads/SimpleBlockingQueue.java
@@ -11,8 +11,11 @@ public class SimpleBlockingQueue<T> {
     @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
     private final Object monitor = this;
-    private int currSize = 0;
     private int maxSize;
+
+    public SimpleBlockingQueue() {
+        this.maxSize = Integer.MAX_VALUE;
+    }
 
     public SimpleBlockingQueue(int maxSize) {
         this.maxSize = maxSize;
@@ -20,22 +23,20 @@ public class SimpleBlockingQueue<T> {
 
     public void offer(T value) throws InterruptedException {
         synchronized (monitor) {
-            while (currSize > maxSize) {
+            while (queue.size() > maxSize) {
                 monitor.wait();
             }
             queue.offer(value);
             monitor.notifyAll();
-            currSize++;
         }
     }
 
     public T poll() throws InterruptedException {
         synchronized (monitor) {
-            while (currSize < 1) {
+            while (queue.size() < 1) {
                 monitor.wait();
             }
             monitor.notifyAll();
-            currSize--;
             return queue.poll();
         }
     }
