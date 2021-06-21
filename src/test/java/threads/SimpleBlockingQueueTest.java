9406a763c88b8b8e9db68cce281efe4f96598e03
AlexanderBanar <s595659@mail.ru>
2021-06-21 07:58:07 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/threads/SimpleBlockingQueueTest.java
+++ src/test/java/threads/SimpleBlockingQueueTest.java
@@ -2,39 +2,87 @@ package threads;
 
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
 public class SimpleBlockingQueueTest {
-//    @Test
-//    public void whenSending4ElementsAndBlockingProducer() throws InterruptedException {
-//        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-//        AtomicInteger result = new AtomicInteger();
-//        AtomicInteger valueOfFour = new AtomicInteger();
-//        Thread producer = new Thread(
-//                () -> {
-//                    queue.offer(1);
-//                    queue.offer(2);
-//                    queue.offer(3);
-//                    queue.offer(4);
-//                }
-//        );
-//        Thread consumer = new Thread(
-//                () -> {
-//                    result.addAndGet(queue.poll());
-//                    result.addAndGet(queue.poll());
-//                    result.addAndGet(queue.poll());
-//                    valueOfFour.addAndGet(queue.poll());
-//                }
-//        );
-//        producer.start();
-//        consumer.start();
-//        producer.join();
-//        consumer.join();
-//        assertThat(result.get(), is(6));
-//        assertThat(valueOfFour.get(), is(4));
-//    }
+    @Test
+    public void whenSending5Elements() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            x -> {
+                                try {
+                                    queue.offer(x);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
 
+    @Test
+    public void whenSendingNothing() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        List<Integer> emptyIntList = new ArrayList<>();
+        Thread producer = new Thread(
+                () -> {
+                    emptyIntList.forEach(
+                            x -> {
+                                try {
+                                    queue.offer(x);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(new CopyOnWriteArrayList<>()));
+    }
 }
