c56524977e518aaa8d70fa1fca3882a7702fd691
AlexanderBanar <s595659@mail.ru>
2021-06-24 07:52:39 +0300

    1. Неблокирующий кеш [#4741] _ part 1

---
+++ src/main/java/threads/cache/Cache.java
@@ -0,0 +1,27 @@
+package threads.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        Base stored = memory.get(model.getId());
+        if (stored.getVersion() != model.getVersion()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        Base modelNewVersioned = new Base(model.getId(), model.getVersion() + 1);
+        modelNewVersioned.setName(model.getName());
+        return memory.computeIfPresent(model.getId(),
+                (key, value) -> modelNewVersioned) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId(), model);
+    }
+}
